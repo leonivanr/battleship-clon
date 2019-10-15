e29bfa090d6d86dfdf3c1242e5a5edeae23008c8
Leon <leonivanrodrigo@gmail.com>
2019-10-15 15:09:35 -0300

    WIP. Add return option.

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -8,13 +8,16 @@ const updateGamesTable = data => {
   var htmlList = data.games.map(function (game) {
     let gamePlayersHtml;
     let joinBtn = "";
+    console.log(game.id)
     if (game.game_players.length == 1) {
-      joinBtn = '<td><form class="text-center" id="createGameForm"><input class="pixel-sdw" type="submit" id="" value="JOIN" /></form></td>';
+      joinBtn = '<td><form class="text-center" id="createGameForm"><input class="pixel-sdw return-game" type="submit" id="gameId' +
+        game.id + '" value="JOIN" /></form></td>';
       gamePlayersHtml = '<td>' + game.game_players[0].player.email + '</td>' +
         '<td>awaiting for player...</td>';
     } else {
       if (game.game_players[0].player.id == data.player.id || game.game_players[1].player.id == data.player.id) {
-        joinBtn = '<td><form class="text-center" id="createGameForm"><input class="pixel-sdw" type="submit" id="" value="JOIN" /></form></td>'
+        joinBtn = '<td><form class="text-center" id="createGameForm"><input class="pixel-sdw join-game" type="submit" id="gameId' +
+          game.id + '" value="JOIN" /></form></td>'
       }
       gamePlayersHtml = game.game_players.map(function (p) {
         return '<td>' + p.player.email + '</td>'
@@ -54,6 +57,46 @@ newgameBtn.addEventListener('click', (e) => {
       console.log("Miren mi juego ", data);
       console.log("game created");
       gameViewUrl = "/web/game.html?gp=" + data.gpid;
+      // $('#gameCreatedSuccess').show("slow").delay(2000).hide("slow");
+      setTimeout(
+        function () {
+          location.href = gameViewUrl;
+        }, 3000);
+    })
+    .fail((error) => {
+      console.log('not ok');
+    })
+})
+
+$('#game-list-table').on('click', '.join-game', function (e) {
+  e.preventDefault();
+  console.log(e.target.id);
+  let gameId = e.target.id.replace(/\D+/g, '');
+  console.log(gameId)
+  $.post("/api/games/" + gameId + "/players/")
+    .done((data) => {
+      console.log(data);
+      // gameViewUrl = "/web/game.html?gp=" + data.gpid;
+      // $('#gameCreatedSuccess').show("slow").delay(2000).hide("slow");
+      setTimeout(
+        function () {
+          location.href = gameViewUrl;
+        }, 3000);
+    })
+    .fail((error) => {
+      console.log('not ok');
+    })
+})
+
+$('#game-list-table').on('click', '.return-game', function (e) {
+  e.preventDefault();
+  console.log(e.target.id);
+  let gameId = e.target.id.replace(/\D+/g, '');
+  console.log(gameId)
+  $.get("/api/games/" + gameId + "/players/")
+    .done((data) => {
+      console.log(data);
+      // gameViewUrl = "/web/game.html?gp=" + data.gpid;
       // $('#gameCreatedSuccess').show("slow").delay(2000).hide("slow");
       setTimeout(
         function () {
