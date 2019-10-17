1a98a51677a63f54ae42e12024c67feb196f9fa6
Leon <leonivanrodrigo@gmail.com>
2019-10-17 02:21:55 -0300

    Fix join, return & create game.

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -1,112 +1,121 @@
+$('#gameCreatedSuccess, #gameJoined, #gameReEnter').hide();
 const newgameBtn = document.querySelector('#createGame');
-var games;
-var playerId;
+const dateOptions = {
+  year: '2-digit',
+  month: '2-digit',
+  day: 'numeric'
+}
+let games;
+let playerId;
+
 const updateUserName = data => {
   let userData = document.querySelector('#user-name');
-  userData.innerText = 'Welcome ' + data.player.email + '!';
+  userData.innerText = `Hi ${data.player.email}!`;
 }
+
 const updateGamesTable = data => {
   var htmlList = data.games.map(game => {
     let gamePlayersHtml;
     let stateBtn = "";
-    console.log(game.id)
     if (game.game_players.length == 1) {
-      stateBtn = '<td><form class="text-center" id="createGameForm"><input class="pixel-sdw join-game" type="submit" id="gameId' + game.id + '" value="JOIN" /></form></td>';
-      gamePlayersHtml = '<td>' + game.game_players[0].player.email + '</td>' +
-        '<td>awaiting for player...</td>';
+      if (game.game_players[0].player.id == data.player.id) {
+        stateBtn = `<td><form class="text-center"><input class="pixel-sdw return-game" type="submit" id="gameId${game.id}" value="RETURN" /></form></td>`
+      } else {
+        stateBtn = `<td><form class="text-center"><input class="pixel-sdw join-game" type="submit" id="gameId${game.id}" value="JOIN" /></form></td>`;
+      }
+      gamePlayersHtml = `<td>${game.game_players[0].player.email}</td><td>awaiting for player...</td>`;
     } else {
       if (game.game_players[0].player.id == data.player.id || game.game_players[1].player.id == data.player.id) {
-        stateBtn = '<td><form class="text-center" id="createGameForm"><input class="pixel-sdw return-game" type="submit" id="gameId' + game.id + '" value="RETURN" /></form></td>'
+        stateBtn = `<td><form class="text-center"><input class="pixel-sdw return-game" type="submit" id="gameId${game.id}" value="RETURN" /></form></td>`
       }
       gamePlayersHtml = game.game_players.map(p => {
-        return '<td>' + p.player.email + '</td>'
+        return `<td>${p.player.email}</td>`
       }).join('');
     }
 
-
-
-    return '<tr class="mb-3"><td>' + new Date(game.created_date).toLocaleString('es-AR', {
-      year: '2-digit',
-      month: '2-digit',
-      day: 'numeric'
-    }) + '</td>' + gamePlayersHtml + stateBtn + '</tr>';
+    return '<tr class="mb-3"><td>' + new Date(game.created_date).toLocaleString('es-AR', dateOptions) + '</td>' + gamePlayersHtml + stateBtn + '</tr>';
   }).join('');
   document.getElementById("game-list-table").innerHTML = htmlList;
 }
+
 // load and display JSON sent by server for /players
 const loadData = () => {
   $.get("/api/games")
-    .done(function (data) {
-      console.log(data)
+    .done(data => {
+      console.log('First load data:', data)
       updateUserName(data);
       updateGamesTable(data);
       games = data.games;
       playerId = data.player.id;
     })
-    .fail(() => {
-      console.log("not ok")
+    .fail(error => {
+      console.log(error);
     });
 }
 
 loadData();
+
 // Listeners
 newgameBtn.addEventListener('click', (e) => {
   e.preventDefault();
   $.post("/api/games")
-    .done((data) => {
+    .done(data => {
       console.log("Miren mi juego ", data);
       console.log("game created");
       gameViewUrl = "/web/game.html?gp=" + data.gpid;
-      // $('#gameCreatedSuccess').show("slow").delay(2000).hide("slow");
+      $('#createGameForm').hide();
+      $('#gameCreatedSuccess').show("slow");
       setTimeout(
         function () {
           location.href = gameViewUrl;
         }, 3000);
     })
-    .fail((error) => {
-      console.log('not ok');
+    .fail(error => {
+      console.log(error);
     })
 })
 
-$('#game-list-table').on('click', '.join-game', function (e) {
+$('#game-list-table').on('click', '.join-game', (e) => {
   e.preventDefault();
-  console.log(e.target.id);
+  // Get gameid from button. 
   let gameId = e.target.id.replace(/\D+/g, '');
-  console.log(gameId)
+
   $.post("/api/games/" + gameId + "/players/")
-    .done((data) => {
+    .done(data => {
       console.log(data);
-      // gameViewUrl = "/web/game.html?gp=" + data.gpid;
-      // $('#gameCreatedSuccess').show("slow").delay(2000).hide("slow");
+      $('#createGameForm').hide();
+      $('#gameJoined').show("slow");
+      gameViewUrl = "/web/game.html?gp=" + data.gpid;
       setTimeout(
         function () {
           location.href = gameViewUrl;
         }, 3000);
     })
-    .fail((error) => {
-      console.log('not ok');
+    .fail(error => {
+      console.log(error);
     })
 })
 
-$('#game-list-table').on('click', '.return-game', function (e) {
+$('#game-list-table').on('click', '.return-game', (e) => {
   e.preventDefault();
-  console.log(e.target.id);
-  let gameId = e.target.id.replace(/\D+/g, '');
-  console.log('elemento: gameId', gameId)
-  console.log(games)
-  console.log(games.filter((game) => (game.id == gameId)));
 
-  // $.get("/api/game_view/"+gpId)
-  //   .done((data) => {
-  //     console.log(data);
-  //     // gameViewUrl = "/web/game.html?gp=" + data.gpid;
-  //     // $('#gameCreatedSuccess').show("slow").delay(2000).hide("slow");
-  //     setTimeout(
-  //       function () {
-  //         location.href = gameViewUrl;
-  //       }, 3000);
-  //   })
-  //   .fail((error) => {
-  //     console.log('not ok');
-  //   })
+  // Get gameid from button. 
+  let gameId = e.target.id.replace(/\D+/g, '');
+  //Get gameplayer ID matching player's id. 
+  let gameviewGP = games.filter(game => (game.id == gameId))[0].game_players.filter(gpl => (gpl.player.id == playerId))[0].gpid;
+
+  $.get("/api/game_view/" + gameviewGP)
+    .done(data => {
+      console.log(data);
+      gameViewUrl = "/web/game.html?gp=" + gameviewGP;
+      $('#createGameForm').hide();
+      $('#gameReEnter').show("slow").delay(2000).hide("slow");
+      setTimeout(
+        function () {
+          location.href = gameViewUrl;
+        }, 3000);
+    })
+    .fail(error => {
+      console.log(error);
+    })
 })
